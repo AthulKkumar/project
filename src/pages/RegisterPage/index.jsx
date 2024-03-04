dd70cf1ed549947984d2a5cecd47285975b507c1
AthulKkumar <athulkkumar5@gmail.com>
2024-03-04 18:42:46 +0530

    enh(app): added register and onbarding pages

---
+++ src/pages/RegisterPage/index.jsx
@@ -0,0 +1,160 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import sigin from "../../assets/svg/sigin.svg";
+
+const Signin = () => {
+  const [forgetPassword, setForgetPassword] = useState(false);
+  return (
+    <div className="container-fluid min-vh-100 d-flex text-light ">
+      <div className="container-fluid  w-100  min-vh-100  "></div>
+      <div
+        className="container-fluid  min-vh-100 "
+        style={{
+          background:
+            "linear-gradient(45deg, rgba(235, 124, 73, 1), rgba(240, 79, 82, 1))",
+        }}
+      ></div>
+
+      {forgetPassword ? (
+        <ForgetPassword />
+      ) : (
+        <div className="container bg-white position-absolute  top-50 start-50 translate-middle d-flex p-2 rounded w-60 min-vh-80 border">
+          <div className="container bg-primary w-50 d-flex flex-column justify-content-start align-items-start  p-5 gap-4 rounded">
+            <img src="/logo.svg" alt="" srcset="" />
+            <div>
+              <h1>Welcome Back</h1>
+              <p>
+                Login and see your performance and keep your learning curve like
+                no one.
+              </p>
+            </div>
+            <section className="w-100 d-flex flex-column justify-content-center align-items-center mb-2">
+              <img src={sigin} alt="logo" srcset="" className="w-50 mb-5" />
+            </section>
+          </div>
+
+          <div className="conatiner w-50 d-flex flex-column justify-content-center align-items-center gap-3 text-black">
+            <section className="d-flex flex-column justify-content-center align-items-center">
+              <h1>Sigin In</h1>
+              <p>Let’s open your skill repository.</p>
+            </section>
+
+            <section className="d-flex flex-column justify-content-center align-items-stretch w-50">
+              <div className="mb-3">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="formGroupExampleInput"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="formGroupExampleInput2"
+                  placeholder="Password"
+                />
+              </div>
+              <button type="button" className="btn btn-primary">
+                Sign In
+              </button>
+            </section>
+
+            <div className="d-flex justify-content-between gap-5 ">
+              <div className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  value=""
+                  id="flexCheckChecked"
+                  checked
+                />
+                <label className="form-check-label" for="flexCheckChecked">
+                  Keep me signed in
+                </label>
+              </div>
+              <label
+                className="form-check-label"
+                for="flexCheckChecked"
+                onClick={(e) => setForgetPassword(true)}
+              >
+                Forgot Password ?
+              </label>
+            </div>
+
+            <h6>Don’t have an account? Contact Administrator</h6>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+function ForgetPassword() {
+  const [email, setEmail] = useState(true);
+  const navigate = useNavigate();
+
+  return (
+    <div className="container bg-white position-absolute  top-50 start-50 translate-middle d-flex p-2 rounded w-100 min-vh-80 border text-black">
+      <div className="d-flex flex-column justify-content-between align-items-start p-4 gap-3 w-100">
+        <button
+          className="btn btn-secondary"
+          onClick={() => navigate("/register")}
+        >
+          Go Back
+        </button>
+        <section className="d-flex flex-column justify-content-center align-items-center gap-3 mb-5 w-100 ">
+          <img src="/forgotpassword.svg" alt="" />
+          <h1>Forget Password ?</h1>
+          {email ? (
+            <>
+              <p>
+                Don't worry! Resetting your password is simple. Just type in the
+                email you registered to Record.
+              </p>
+              <section className="d-flex flex-column justify-content-center align-items-strech w-25">
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="formGroupExampleInput"
+                    placeholder="Email"
+                  />
+                </div>
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={(e) => setEmail(false)}
+                >
+                  Send
+                </button>
+              </section>
+              <p>Don’t have an account? Contact Administrator</p>
+            </>
+          ) : (
+            <>
+              <p>Great, password reset link has been sent to your email.</p>
+              <section className="d-flex flex-column justify-content-center align-items-strech w-25">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  onClick={() => navigate("/register")}
+                >
+                  Sign In
+                </button>
+              </section>
+            </>
+          )}
+        </section>
+
+        <section className="d-flex justify-content-between w-100">
+          <p>Privacy policy</p>
+          <p>CopyRights 2022</p>
+        </section>
+      </div>
+    </div>
+  );
+}
+
+export default Signin;
