fc57429824f3a741bcf70f4daa2d800fbe06ec12
Devdatt Gautam <115690132+Devdatt-gautam@users.noreply.github.com>
2024-03-06 10:09:18 +0530

    Profile (#6)
    * Completed Basic Profile edit  Layout

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -1,7 +1,54 @@
-import React from "react";
-
+import { useState } from "react";
+import userImage from "../../assets/svg/userImage.svg";
+import BasicProfile from "../../components/BasicProfile";
 const Profile = () => {
-  return <div>Its From Profile</div>;
+  const [profileType, setProfileType] = useState("basic");
+  return (
+    <div className="row">
+      <div
+        className="col col-4 rounded-4 pt-2"
+        style={{ border: "1px solid #1212124D" }}
+      >
+        <div className="row d-flex flex-column  align-items-center text-center ">
+          <img
+            src={userImage}
+            alt="profile"
+            style={{ width: "50%", height: "50%" }}
+            className="rounded-circle"
+          />
+          <h4
+            style={{
+              fontWeight: "700",
+
+              color: "#12131A",
+            }}
+          >
+            Akshay Kumar
+          </h4>
+        </div>
+        <ul
+          style={{
+            textTransform: "capitalize",
+            color: "#8F8F8F",
+            fontWeight: "500",
+            fontSize: "20px",
+          }}
+          className="d-flex flex-column gap-4 pt-4 "
+        >
+          <li>Basic Profile</li>
+          <li>Education</li>
+          <li>Work Experience</li>
+          <li>Licenses & Certifications</li>
+          <li>Skill Repository</li>
+          <li>Projects</li>
+          <li>other activities</li>
+        </ul>
+      </div>
+      <div className="col col-8 px-4">
+        <div className="row">{profileType === "basic" && <BasicProfile />}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
