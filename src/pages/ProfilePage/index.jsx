f1406b1b4889c99d7b1a2d5855eae75ef2e98d29
Devdatt Gautam <115690132+Devdatt-gautam@users.noreply.github.com>
2024-03-07 10:21:07 +0530

    created basic  ui for all profile section (#9)

--- src/pages/ProfilePage/index.jsx
+++ src/pages/ProfilePage/index.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 import userImage from "../../assets/svg/userImage.svg";
 import BasicProfile from "../../components/BasicProfile";
+import EducationProfile from "../../components/EducationProfile";
+import WorkProfile from "../../components/WorkProfile";
+import OtherProfile from "../../components/OtherProfile";
+import LicenseProfile from "../../components/LicenseProfile";
+import ProjectProfile from "../../components/ProjectProfile";
+import "./ProfilePage.css";
+import SkillProfile from "../../components/SkillProfile";
 const Profile = () => {
   const [profileType, setProfileType] = useState("basic");
   return (
     <div className="row">
       <div
         className="col col-4 rounded-4 pt-2"
-        style={{ border: "1px solid #1212124D" }}
+        style={{ border: "1px solid #1212124D", minHeight: "80vh" }}
       >
         <div className="row d-flex flex-column  align-items-center text-center ">
           <img
@@ -35,54 +42,71 @@ const Profile = () => {
           }}
           className="d-flex flex-column gap-4 pt-4 "
         >
-
           <li
             style={{ cursor: "pointer" }}
             onClick={() => setProfileType("basic")}
+            className={profileType == "basic" ? "colored" : " "}
           >
             Basic Profile
           </li>
           <li
             style={{ cursor: "pointer" }}
             onClick={() => setProfileType("education")}
+            className={profileType == "education" ? "colored" : " "}
           >
             Education
           </li>
           <li
             style={{ cursor: "pointer" }}
             onClick={() => setProfileType("work")}
+            className={profileType == "work" ? "colored" : " "}
           >
             Work Experience
           </li>
           <li
             style={{ cursor: "pointer" }}
             onClick={() => setProfileType("license")}
+            className={profileType == "license" ? "colored" : " "}
           >
             Licenses & Certifications
           </li>
           <li
             style={{ cursor: "pointer" }}
             onClick={() => setProfileType("skill")}
+            className={profileType == "skill" ? "colored" : " "}
           >
             Skill Repository
           </li>
           <li
             style={{ cursor: "pointer" }}
             onClick={() => setProfileType("project")}
+            className={profileType == "project" ? "colored" : " "}
           >
             Projects
           </li>
           <li
             style={{ cursor: "pointer" }}
             onClick={() => setProfileType("other")}
+            className={profileType == "other" ? "colored" : " "}
           >
             other activities
           </li>
-
         </ul>
       </div>
       <div className="col col-8 px-4">
         <div className="row">{profileType === "basic" && <BasicProfile />}</div>
+        <div className="row">
+          {profileType === "education" && <EducationProfile />}
+        </div>
+        <div className="row">{profileType === "work" && <WorkProfile />}</div>
+        <div className="row">
+          {profileType === "license" && <LicenseProfile />}
+        </div>
+        <div className="row">{profileType === "skill" && <SkillProfile />}</div>
+        <div className="row">
+          {profileType === "project" && <ProjectProfile />}
+        </div>
+        <div className="row">{profileType === "other" && <OtherProfile />}</div>
       </div>
     </div>
   );
