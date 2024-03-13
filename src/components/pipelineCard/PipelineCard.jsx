e62cb6a49d7e4d71be194fa9067bd7fe60c8b864
Thilak <thilaknanu101@gmail.com>
2024-03-13 12:18:59 +0530

    css-fixed

--- src/components/pipelineCard/PipelineCard.jsx
+++ src/components/pipelineCard/PipelineCard.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import "./pipelineCard.css";
 import thumbnail from "../../assets/thumbnail.webp";
+import { HiOutlineDotsVertical } from "react-icons/hi";
 
-const PipelineCard = () => {
+const PipelineCard = ({ img, company, course, accadamy }) => {
   return (
     <div className="pipelineCard">
-      <div className="pipelineCard__img">
-        <img src={thumbnail} alt="" width={"150px"} />
+      <div className="pipelineCard__wrapper">
+        <div className="pipelineCard__img">
+          <img src={thumbnail} alt="" width={"150px"} />
+        </div>
+        <div className="pipelineCard__text">
+          <h6>{course}</h6>
+          <small>{accadamy}</small>
+          <div>
+            <img src={img} width={"25px"} alt="" />
+            <p>{company}</p>
+          </div>
+        </div>
       </div>
-      <div className="pipelineCard__text">
-        <h6>UI/UX Fundamentals</h6>
-        <small>Ran Segal</small>
-        <p>Udemy</p>
+
+      <div>
+        <HiOutlineDotsVertical fontSize={30} />
       </div>
     </div>
   );
